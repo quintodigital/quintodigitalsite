2b8de68e53967b67255104f7ab2d4893c6eb23b1
Emerson Souza <emersonwebdev@gmail.com>
2024-05-04 11:41:25 -0300

    initial commit

---
+++ obfuscator-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  compact: true,
+  selfDefending: false,
+  debugProtection: true,
+  // debugProtectionInterval: true,
+  numbersToExpressions: true,
+  splitStrings: true,
+};
